2e8458cd12ee7e5d1cdd1b500681e48414541201
ikromjon0102 <ikromjon01021995@gmail.com>
2024-06-04 00:11:18 +0500

    4 ta til, error va success count qo'shildi

--- problems/models.py
+++ problems/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+# class Problem(models.Model):
+#     title = models.CharField(max_length=255)
+#     description = models.TextField()
+#     input_format = models.TextField()
+#     output_format = models.TextField()
+#     sample_input = models.TextField()
+#     sample_output = models.TextField()
+#     constraints = models.TextField()
+#
+#     def __str__(self):
+#         return self.title
+
+
+# problems/models.py
+from django.db import models
+
 class Problem(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -7,7 +23,7 @@ class Problem(models.Model):
     output_format = models.TextField()
     sample_input = models.TextField()
     sample_output = models.TextField()
-    constraints = models.TextField()
+    test_cases = models.TextField()  # Test holatlarini saqlash uchun maydon
 
     def __str__(self):
         return self.title
--- user_code.py
+++ user_code.py
@@ -1,1 +1,8 @@
-console.log(5)
+a,b = input().split()
+a,b = int(a), int(b)
+if a == b:
+  print('=')
+elif a<b:
+  print('>')
+else:
+  print('>')
